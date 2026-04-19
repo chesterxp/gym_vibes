a4bdc14bbebc0a0fa8e8d4bcd214d092c0e0207b
Łukasz Zawiślak <lukasz.zawislak@agora.pl>
2026-04-19 13:12:20 +0200

    Fixed menu. Section background. FAQ.

--- src/main.ts
+++ src/main.ts
@@ -3,36 +3,48 @@ import './style.scss'
 console.log('Gym Vibes');
 
 const init = () => {
-  // const isMobileVariant = isMobile();
+  const isMobileVariant = isMobile();
+  const menu: HTMLElement | null = document.querySelector('.nav__list');
+
   addFAQEvents();
-  addEventForMenuButton();
+  if(menu && isMobileVariant) {
+    addEventForMenuButton(menu);
+    addEventForMenuLinks(menu);
+  }
 }
 
 const addFAQEvents = () => {
   const items = document.querySelectorAll('.fag_item');
   items.forEach(item => {
-      const btn = item.querySelector('.faq_btn');
       const text = item.querySelector('.faq_text');
-      if(btn && text) {
-          btn.addEventListener('click', () => {
+      if(item && text) {
+          item.addEventListener('click', () => {
               text.classList.toggle('hidden');
           });
       }
   });
 }
 
-const addEventForMenuButton = () => {
+const addEventForMenuButton = (menu: HTMLElement) => {
   const btn = document.querySelector('.menu__btn');
-  const menu = document.querySelector('.nav__list');
-
   btn?.addEventListener('click', () => {
       menu?.classList.toggle('nav__list--show');
   });
 
 }
 
-// const isMobile = () => {
-//   return window.innerWidth <= 944;
-// }
+const addEventForMenuLinks = (menu: HTMLElement) => {
+  const links = document.querySelectorAll('.nav__link');
+  links.forEach(link => {
+      link.addEventListener('click', () => {
+          menu?.classList.remove('nav__list--show');
+      });
+  });
+
+}
+
+const isMobile = () => {
+  return window.innerWidth <= 944;
+}
 
 init();
